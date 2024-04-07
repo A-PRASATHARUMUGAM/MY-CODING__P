bc1046402c6b07ba66fdf281518a5b5b5548e7f8
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-04-07 13:09:51 +0530

    create arithmetic operation in js

--- PRATICE CODING/P1.js
+++ PRATICE CODING/P1.js
@@ -1,11 +1,18 @@
 
+
+
 function convert(){
 
-
-    let cm = document.getElementById("input").value;
-    let  inch=cm/2.54;
-    let result=document.getElementById("result") ;
-    result.innerHTML= inch
-
-}
-
+    let a = Number( document.getElementById("input1").value);
+    let b = Number( document.getElementById("input2").value);
+     let c= a+b;
+     let d= a-b;
+     let e= a*b;
+     let f= a/b;
+     let g= a%b;
+     let result=document.getElementById("result");
+      result.innerHTML= "Addition " + c +" <br> subtraction "+d+"<br> Multiplication "+e+"<br> Division "+f+ 
+      "<br> Modular Division" +g;
+                     
+ }
+ 
