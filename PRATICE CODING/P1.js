a345c0d5e59dc5717bba2487ce648e251760b16b
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-04-09 09:21:36 +0530

    finshed the celcius to fahrenheit calculation

--- PRATICE CODING/P1.js
+++ PRATICE CODING/P1.js
@@ -1,4 +1,7 @@
 function convert(){
 
-  const c=Number(document.getElementById("input1".value))
+  const c=Number(document.getElementById("input1").value)
+  const f=(c * 9/5)+32+"F";
+  const result=document.getElementById("result");
+  result.innerHTML= f ;
 }
