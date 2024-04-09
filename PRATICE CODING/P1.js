38f95407fe912db3ebc5220086f0818329637bee
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-04-09 09:13:19 +0530

    creating celcius to fahrenheit calculation

--- PRATICE CODING/P1.js
+++ PRATICE CODING/P1.js
@@ -1,18 +1,4 @@
-
-
-
 function convert(){
 
-    let a = Number( document.getElementById("input1").value);
-    let b = Number( document.getElementById("input2").value);
-     let c= a+b;
-     let d= a-b;
-     let e= a*b;
-     let f= a/b;
-     let g= a%b;
-     let result=document.getElementById("result");
-      result.innerHTML= "Addition " + c +" <br> subtraction "+d+"<br> Multiplication "+e+"<br> Division "+f+ 
-      "<br> Modular Division" +g;
-                     
- }
- 
+  const c=Number(document.getElementById("input1".value))
+}
