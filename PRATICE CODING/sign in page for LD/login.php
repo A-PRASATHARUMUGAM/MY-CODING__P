e902c901efa9bd16455ea20e43c2fb443a8b9def
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-04-11 22:04:34 +0530

    finished the sign and registration page

--- PRATICE CODING/sign in page for LD/login.php
+++ PRATICE CODING/sign in page for LD/login.php
@@ -2,40 +2,33 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>
-		Login and Registration
-	</title>
-	
-	<link rel="stylesheet" type="text/css" href="../css/sign.css">
+	<title> Login </title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="signin.css">
 </head>
 <body>
-	<div class="header">
-		<h2>Login Here!</h2>
-	</div>
-	
-	<form method="post" action="login.php">
+
+	<form method="post" action="login.php" class="form-container">
+		<h2 class="form-title">Sign up</h2>
 
 		<?php include('error.php'); ?>
 
 		<div class="input-group">
-			<label>Enter Username</label>
-			<input type="text" name="username" >
+	
+			<input type="text" name="username"  placeholder="Enter username" required>
 		</div>
 		<div class="input-group">
-			<label>Enter Password</label>
-			<input type="password" name="password">
+			<input type="password" name="password" placeholder="Enter password" required>
 		</div>
 		<div class="input-group">
-			<button type="submit" class="btn" name="login_user">Login </button>
+			<button type="submit" class="btn" name="login_user" >Login </button>
 		</div>
 		
 
 
 <p>
-			New Here? 
-			<a href="register.php">
-				Click here to register!
-			</a>
+			 	New Here? 
+			<a href="register.php"> Click here to register !</a>
 		</p>
 
 
