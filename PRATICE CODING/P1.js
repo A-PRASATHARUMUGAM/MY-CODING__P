ea8882ab538a4032b499f0bb9c4a6d7e78df5ef4
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-05-17 13:33:58 +0530

    pratice template litrails

--- PRATICE CODING/P1.js
+++ PRATICE CODING/P1.js
@@ -1,5 +1,15 @@
-function click() {
-  let c = document.getElementsByClassName("input1").value;
-  let r = document.getElementByClassName("result");
-  r.innerHTML = c;
+function goTo() {
+  let c = document.getElementById("input1").value;
+
+  let  f=c[Math.floor(Math.random()*6)+1]; 
+  let r = document.getElementById("result");
+  r.innerHTML =f;
+
+
 }
+fname="prasath"
+lname="arumugam"
+
+console.log(fname +" "+ lname);
+
+console.log(`${fname} ${lname}`);
