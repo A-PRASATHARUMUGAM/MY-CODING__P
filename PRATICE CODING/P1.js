42f16968f2a84f10e85ee0ac3b4a2c5d68468915
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-05-19 14:46:06 +0530

    pratice js variable of scope

--- PRATICE CODING/P1.js
+++ PRATICE CODING/P1.js
@@ -1,15 +1,32 @@
 function goTo() {
   let c = document.getElementById("input1").value;
 
-  let  f=c[Math.floor(Math.random()*6)+1]; 
+  let f = c[Math.floor(Math.random() * 6) + 1];
   let r = document.getElementById("result");
-  r.innerHTML =f;
+  r.innerHTML = f;
 
 
 }
-fname="prasath"
-lname="arumugam"
+console.error("custom Error")
+console.warn("custom warning")
+console.clear();
 
-console.log(fname +" "+ lname);
 
-console.log(`${fname} ${lname}`);
+console.time("Timer");
+
+for (i = 0; i < 10; i++) {
+  console.log()
+}
+console.timeEnd("Timer");
+
+
+
+const student=({"name":"prasath","age":20});
+console.table(student);
+console.log(student.name);
+student.name="sabari";
+console.table(student);
+
+
+var a=Symbol();
+console.log(typeof(a));
