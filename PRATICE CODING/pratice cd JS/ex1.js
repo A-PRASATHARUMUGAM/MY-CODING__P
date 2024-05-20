68958766c206a5c57525e10c38dfefef625740f3
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-05-20 12:56:13 +0530

    create celsius to fahrenheit converter

--- PRATICE CODING/pratice cd JS/ex1.js
+++ PRATICE CODING/pratice cd JS/ex1.js
@@ -1,1 +1,10 @@
-console.log("ex1")
+
+function convert(){
+
+    var celsius=document.getElementById("celsius-input").value;
+     var fahrenheit=(celsius*9/5)+32;
+        fahresult=document.getElementById("fahresult");
+        fahresult.innerHTML = fahrenheit; 
+
+}   
+
