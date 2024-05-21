fc4fd55219c25cd33a380410ed6f7945cb8623c5
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-05-21 22:39:09 +0530

    not finishing bitwise operation

--- PRATICE CODING/pratice cd JS/ex3.js
+++ PRATICE CODING/pratice cd JS/ex3.js
@@ -3,10 +3,15 @@
 function convert3(){
 let a=Number(document.getElementById("bitinput1").value);
 let b=Number(document.getElementById("bitinput2").value);
-bitresult=document.getElementById("bitresult");
-bitresult.innerHTML= "and"+(a&b)+"<br>"+"or"+(a|b);
+document.getElementById("bitresult").innerHTML= "and"+(a&b)+"<br>"+"or"+(a|b); 
 
 }
+
+a=5;
+b=2;
+ a<<=b
+ console.log(a);
+ console.log(b);
 
 
 
