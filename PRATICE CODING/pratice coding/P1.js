b028f5bcfb6ba64a3cf3b93315df982e07d12785
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-06-04 13:11:43 +0530

    using multiple condition or condition chains

--- PRATICE CODING/pratice coding/P1.js
+++ PRATICE CODING/pratice coding/P1.js
@@ -1,3 +1,9 @@
-function myfunction(){
-    console.log("I am come back in javascript")
+function myfunction() {
+
+    let mark = document.getElementById("input1").value;
+    let Grade=mark>=90?"Grade A":mark>=80?"Grade B":"Grade C"
+    let result = document.getElementById("result");
+    result.innerHTML = Grade;
+
+
 }
