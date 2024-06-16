b4f198a698c68a48bfd6aa1b75f9b131f5acbe72
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-06-16 22:34:20 +0530

    finished the responsive navbar in portfolio using P1.HTML

--- PRATICE CODING/pratice coding/P1.js
+++ PRATICE CODING/pratice coding/P1.js
@@ -1,19 +1,6 @@
-function myfunction() {
-
-    let mark = document.getElementById("input1").value;
-    let Grade=mark>=90?"Grade A":mark>=80?"Grade B":"Grade C"
-    let result = document.getElementById("result");
-    result.innerHTML = Grade;
-
-
-}
-
-let nums=[];
-for(let i=0;i<3;i++){
-    nums.push([]);
-    for(let j=0;j<3;j++){
-        nums[i].push(j);
-    }
-
-}
-console.table(nums)
+function myfunction(){
+ const menu=document.querySelector(".hmenu");
+ const menuList=document.querySelector(".navbar ul");
+    menuList.classList.toggle("showmenu")
+ 
+}   
