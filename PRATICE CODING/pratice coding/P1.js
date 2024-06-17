0a682df3f176c40d11db44df3a234a16d0ff1d44
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-06-17 10:58:06 +0530

    newely create the folders navbar responsive

--- PRATICE CODING/pratice coding/P1.js
+++ PRATICE CODING/pratice coding/P1.js
@@ -1,6 +1,6 @@
-function myfunction(){
- const menu=document.querySelector(".hmenu");
- const menuList=document.querySelector(".navbar ul");
-    menuList.classList.toggle("showmenu")
- 
-}   
+const menu=document.querySelector(".hmenu");
+const menuList=document.querySelector(".navbar ul");
+menu.addEventListener('click',()=>{
+    menuList.classList.toggle("showmenu");
+    
+})  
