6af04df64c1b3d983ec01c3c728c0468780396bd
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-06-18 17:19:04 +0530

    finished the responsive navbar in home page

---
+++ Portfolio responsive/assets/js/script.js
@@ -0,0 +1,6 @@
+function myfunction(){
+    const menu=document.querySelector(".hmenu");
+    const menuList=document.querySelector(".navbar ul");
+       menuList.classList.toggle("showmenu")
+    
+   }   
