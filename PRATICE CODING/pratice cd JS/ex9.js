322120a101a4835a3cfe49c87620d3015719bfb9
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-06-19 22:14:03 +0530

    pratice the Math function in js

---
+++ PRATICE CODING/pratice cd JS/ex9.js
@@ -0,0 +1,31 @@
+let a,b,c,d;
+// c=Math.round(5.8);
+// c=Math.round(5.4);
+// c=Math.round(-5.2);
+ 
+// c=Math.floor(5.8);
+// c=Math.floor(5.4);
+// c=Math.floor(-5.8);
+// c=Math.floor(-5.4);
+
+// a=Math.ceil(5.8);
+// b=Math.ceil(5.4);
+// c=Math.ceil(-5.8);
+// d=Math.ceil(-5.4);
+
+// a=Math.trunc(2.20);
+// b=Math.trunc(-2.80);
+
+a=Math.sqrt(90);
+
+b=Math.abs(5);
+c=Math.abs(-2);
+
+
+
+ 
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
