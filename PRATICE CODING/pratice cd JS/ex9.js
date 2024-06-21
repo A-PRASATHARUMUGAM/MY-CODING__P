bed2a07765e9eacb6fc6d48c10deba49abc1c539
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-06-21 15:21:07 +0530

    pratice the string methodin javascript

--- PRATICE CODING/pratice cd JS/ex9.js
+++ PRATICE CODING/pratice cd JS/ex9.js
@@ -46,49 +46,132 @@ let a,b,c,d;
 // console.log(e);
 
 
-//                         /*STRING FUNCTION  */
-// let first_name="prasath";
-// let last_name="sabari"
+                        /*STRING FUNCTION  */
+let first_name="prasath";
+let last_name="sabari"
 
-// // concatenation
-// c=first_name+" "+last_name;
-// console.log("concatenation : "+c);
+// concatenation
+c=first_name+" "+last_name;
+console.log("concatenation : "+c);
 
-// // concat
-// c=first_name.concat(" ",last_name);
-// console.log("concat : "+c);
+// concat
+c=first_name.concat(" ",last_name);
+console.log("concat : "+c);
 
-// // Append
-// c="prasath";
-// c+=" sabari";
-// console.log("Append : "+c);
+// Append
+c="prasath";
+c+=" sabari";
+console.log("Append : "+c);
 
-// // Escaping
-// c='She can\'t run';
-// console.log("Escaping: "+c);
+// Escaping
+c='She can\'t run';
+console.log("Escaping: "+c);
 
-// // Length
-// c=first_name.length;
-// console.log("Lenght:" +c);
+// Length
+c=first_name.length;
+console.log("Lenght:" +c);
 
-// // UPPER CASE
-// c=first_name.toUpperCase();
-// console.log("Uppercase: "+c)
+// UPPER CASE
+c=first_name.toUpperCase();
+console.log("Uppercase: "+c)
 
-// //Lowercase 
-// c=first_name.toLowerCase();
-// console.log("Lowercase: "+c);
+//Lowercase 
+c=first_name.toLowerCase();
+console.log("Lowercase: "+c);
 
-// // index find
+// index find
 
-// c=first_name.indexOf("a")
-// console.log("indexOf: "+c);
+c=first_name.indexOf("a")
+console.log("indexOf: "+c);
 
-// c=first_name.lastIndexOf("a")
-// console.log("lastIndexOf: "+c); 
+c=first_name.lastIndexOf("a")
+console.log("lastIndexOf: "+c); 
+
+// 
+c=first_name.charAt(1);
+console.log("charAt 1 : "+c)
  
+c=first_name.charCodeAt(1);
+console.log("charCodeAt 1 : "+c)
+
+//  String Partitioning 
+
+// 1.slice
+// 2.substring
+// 3.substr  -it is Depricated function
 
 
+first_name="0123456789"
+
+// 1. slice(start,end)
+c=first_name.slice(1,4); 
+console.log("slice : "+c);
+
+        // --- slice negative value
+c=first_name.slice(-3); 
+console.log("slice : "+c);
+
+
+
+// 2. substring(start,end)
+b=first_name.substring(1,4); 
+console.log("substring :"+b);
+
+        // --- substring opposite value 
+b=first_name.substring(4,1); 
+console.log("substring : "+b);
+
+
+
+// 3. substr(start,length)
+a=first_name.substr(1,4); 
+console.log("substr : "+a); 
+
+//------------------------------  
+
+//Split()
+first_name="hello world Iam pratice to the javascript programming"
+ 
+c=first_name.split(" ");
+console.log("split using space :"+c);
+console.table(c);
+
+//trim()          -it is using to remove extra space before and after;
+let name1=" prasath "; 
+let name2=" prasath ";
+let name3=" prasath ";
+
+// trime (before and after remove space )
+c=name1.trim();
+console.log(c);
+a=c.length
+console.log(a)
+//trimStart()
+c=name2.trimStart();
+console.log(c);
+a=c.length
+console.log(a)
+//trimEnd()
+c=name3.trimEnd();
+console.log(c);
+a=c.length
+console.log(a)
+
+//replace
+c=first_name.replace("world","prasath")
+console.log(c)
+ 
+// includes     -it is using to find the value in string and return True or False;
+c=first_name.includes("world"); 
+console.log(c); 
+
+//search
+c=first_name.search("world");
+console.log(c);
+
+ 
+ 
+ 
 
  
 
