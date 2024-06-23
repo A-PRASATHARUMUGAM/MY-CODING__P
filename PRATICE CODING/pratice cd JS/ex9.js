a6695e1152969dd1cc75fc1303b6d474fb3ba722
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-06-23 15:38:26 +0530

    finally Iam finished the math and string function with example

--- PRATICE CODING/pratice cd JS/ex9.js
+++ PRATICE CODING/pratice cd JS/ex9.js
@@ -46,132 +46,151 @@ let a,b,c,d;
 // console.log(e);
 
 
-                        /*STRING FUNCTION  */
-let first_name="prasath";
-let last_name="sabari"
+//                         /*STRING FUNCTION  */
+// let first_name="prasath";
+// let last_name="sabari"
 
-// concatenation
-c=first_name+" "+last_name;
-console.log("concatenation : "+c);
+// // concatenation
+// c=first_name+" "+last_name;
+// console.log("concatenation : "+c);
 
-// concat
-c=first_name.concat(" ",last_name);
-console.log("concat : "+c);
+// // concat
+// c=first_name.concat(" ",last_name);
+// console.log("concat : "+c);
 
-// Append
-c="prasath";
-c+=" sabari";
-console.log("Append : "+c);
+// // Append
+// c="prasath";
+// c+=" sabari";
+// console.log("Append : "+c);
 
-// Escaping
-c='She can\'t run';
-console.log("Escaping: "+c);
+// // Escaping
+// c='She can\'t run';
+// console.log("Escaping: "+c);
 
-// Length
-c=first_name.length;
-console.log("Lenght:" +c);
+// // Length
+// c=first_name.length;
+// console.log("Lenght:" +c);
 
-// UPPER CASE
-c=first_name.toUpperCase();
-console.log("Uppercase: "+c)
+// // UPPER CASE
+// c=first_name.toUpperCase();
+// console.log("Uppercase: "+c)
 
-//Lowercase 
-c=first_name.toLowerCase();
-console.log("Lowercase: "+c);
+// //Lowercase 
+// c=first_name.toLowerCase();
+// console.log("Lowercase: "+c);
 
-// index find
+// // index find
 
-c=first_name.indexOf("a")
-console.log("indexOf: "+c);
+// c=first_name.indexOf("a")
+// console.log("indexOf: "+c);
 
-c=first_name.lastIndexOf("a")
-console.log("lastIndexOf: "+c); 
+// c=first_name.lastIndexOf("a")
+// console.log("lastIndexOf: "+c); 
 
-// 
-c=first_name.charAt(1);
-console.log("charAt 1 : "+c)
+// //character find 
+// c=first_name.charAt(1); //-it is resturn the index value to the character 
+// console.log("charAt 1 : "+c)
  
-c=first_name.charCodeAt(1);
-console.log("charCodeAt 1 : "+c)
+// c=first_name.charCodeAt(1); //-it is return the unicode value to the character
+// console.log("charCodeAt 1 : "+c)
 
-//  String Partitioning 
+// //  String Partitioning 
 
-// 1.slice
-// 2.substring
-// 3.substr  -it is Depricated function
-
-
-first_name="0123456789"
-
-// 1. slice(start,end)
-c=first_name.slice(1,4); 
-console.log("slice : "+c);
-
-        // --- slice negative value
-c=first_name.slice(-3); 
-console.log("slice : "+c);
+// // 1.slice
+// // 2.substring
+// // 3.substr  -it is Depricated function
 
 
+// first_name="0123456789"
 
-// 2. substring(start,end)
-b=first_name.substring(1,4); 
-console.log("substring :"+b);
+// // 1. slice(start,end)
+// c=first_name.slice(1,4); 
+// console.log("slice : "+c);
 
-        // --- substring opposite value 
-b=first_name.substring(4,1); 
-console.log("substring : "+b);
+//         // --- slice negative value
+// c=first_name.slice(-3); 
+// console.log("slice negative value : "+c);
 
 
 
-// 3. substr(start,length)
-a=first_name.substr(1,4); 
-console.log("substr : "+a); 
+// // 2. substring(start,end)
+// b=first_name.substring(1,4); 
+// console.log("substring :"+b);
 
-//------------------------------  
+//         // --- substring opposite value 
+// b=first_name.substring(4,1); 
+// console.log("substring opposite value : "+b);
 
-//Split()
-first_name="hello world Iam pratice to the javascript programming"
+
+
+// // 3. substr(start,length)
+// a=first_name.substr(1,4); 
+// console.log("substr : "+a); 
+
+// //------------------------------  
+
+// //Split()
+// first_name="hello world Iam pratice JS"
  
-c=first_name.split(" ");
-console.log("split using space :"+c);
-console.table(c);
+// c=first_name.split(" ");
+// console.log("split using space :"+c);
+// console.table(c);
 
-//trim()          -it is using to remove extra space before and after;
-let name1=" prasath "; 
-let name2=" prasath ";
-let name3=" prasath ";
+// //trim()          -it is using to remove extra space before and after;
+// let name1=" prasath "; 
 
-// trime (before and after remove space )
-c=name1.trim();
-console.log(c);
-a=c.length
-console.log(a)
-//trimStart()
-c=name2.trimStart();
-console.log(c);
-a=c.length
-console.log(a)
-//trimEnd()
-c=name3.trimEnd();
-console.log(c);
-a=c.length
-console.log(a)
+// // trime (before and after remove space )
+// c=name1.trim();
+// console.log("trim :"+c);
+// //trimStart()
+// c=name1.trimStart();
+// console.log("trimStart :"+c);
+// //trimEnd()
+// c=name1.trimEnd();
+// console.log("trimEnd :"+c);  
 
-//replace
-c=first_name.replace("world","prasath")
-console.log(c)
- 
-// includes     -it is using to find the value in string and return True or False;
-c=first_name.includes("world"); 
-console.log(c); 
+// // padStart(lenght,add value) -it is adding something before 
+// let bill="Rs.10";
+// a=bill.padStart(7,"0");
+// console.log("padStart :"+a); 
 
-//search
-c=first_name.search("world");
-console.log(c);
+// // padEnd(lenght,add value)     -it is adding something after 
+// a=bill.padEnd(7,"0");
+// console.log("padEnd :"+a); 
 
+
+// //replace(search value,replace value) 
+// console.log("Before Replace :"+first_name);
+// c=first_name.replace("world","prasath")
+// console.log("After replace :"+c)  
  
  
+// // includes     -it is using to find the value in string and return True or False;
+// c=first_name.includes("world"); 
+// console.log("includes :"+c); 
+
+// //search        -it find the word but return the starting index of the word.
+// c=first_name.search("world");
+// console.log("search :"+c);
+// //search -no words in the string return -1
+// c=first_name.search("not");
+// console.log("No words search :"+c);
+
+
+// //startWidth     -it is check the start of word
+// first_name="prasath"
+// c=first_name.startsWith("p");
+// console.log("startsWidth :"+c);
+
+// //endWith        -it is check the end of word
+// c=first_name.endsWith("p");
+// console.log("endsWith :"+c);
+
+
+
  
+ 
+  
 
  
 
