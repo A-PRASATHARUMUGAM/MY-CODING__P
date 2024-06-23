31f0553f2f7ad19b7633be381d86c00dc9777bc0
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-06-23 17:05:17 +0530

    pratice the template literals and long literals sting then using ASCII function

---
+++ PRATICE CODING/pratice cd JS/ex11.js
@@ -0,0 +1,45 @@
+//                 // Template Literals
+
+// let first_name="prasath";
+// let last_name="sabari"
+// let full_name;
+
+// // using ( + ) operator
+// full_name=first_name+" "+last_name;
+// console.log("using + operator :"+full_name)
+
+// // using backtick
+// full_name=`${first_name} ${last_name}`
+// console.log("using backtick :"+full_name);
+ 
+
+//                 // Long literal string 
+
+// //Method -- 1 
+// let longString="variable is placeholder"
+// + "it is hold the value from variable name"
+// +"agian using to assign the values to calculate then reassigining the values and that"
+// +"is a main reason to using"
+// +"the variable i  programming language";
+// console.log(longString);
+
+// //Method -- 2
+// longString="variable is placeholder\
+// it is hold the value from variable name\
+// agian using to assign the values to calculate then reassigining the values and that\
+// is a main reason to using\
+// the variable i  programming language";
+// console.log(longString);
+
+// //Method --3 
+// longString=`variable is placeholder
+// it is hold the value from variable name
+// agian using to assign the values to calculate then reassigining the values and that
+// is a main reason to using
+// the variable i  programming language`;
+// console.log(longString);
+
+// // Print the ASCII value 
+// let ASCII;
+// ASCII=(String.fromCharCode(65,66,67)); 
+// console.log(ASCII);
