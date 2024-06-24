b23f16e56234d062a327e66a6e259a185b9f83c8
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-06-24 22:50:17 +0530

    pratice palindrome in js

--- PRATICE CODING/pratice cd JS/ex12.js
+++ PRATICE CODING/pratice cd JS/ex12.js
@@ -14,14 +14,34 @@
 // alpha() 
 // console.log(z);   
 
- function sum(a,b){
+//  function sum(a,b){
     
-    let total=0;
-    // for(i=0;i<arguments.length;i++)
-    //     {
-    //         total+=arguments[i];
-    //     }
-   arguments.length;
-   console.log(arguments)
- }  
-console.log(sum());
+//     let total=0;
+//     // for(i=0;i<arguments.length;i++)
+//     //     {
+//     //         total+=arguments[i];
+//     //     }
+//    arguments.length;
+//    console.log(arguments)
+//  }  
+// console.log(sum());
+
+function palindrom(string){
+    reverseString=string.split("").reverse().join('');
+    if(reverseString==string){
+        console.log("It is palindrom :"+string);
+    }
+    else{
+
+        console.log("It is not palindrom :"+string);
+    }
+ 
+}
+palindrom("madam")
+
+let first_name="prasath"
+c=[...first_name].reverse().join('');
+console.log(c)
+c=first_name.split('');
+console.log(c)
+
