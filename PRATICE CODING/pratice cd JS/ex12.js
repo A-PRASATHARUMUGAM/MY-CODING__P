3443d7f61655f27c8981874fdac70006eb05fe60
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-06-24 21:47:52 +0530

    praticte arbitaray function in js

--- PRATICE CODING/pratice cd JS/ex12.js
+++ PRATICE CODING/pratice cd JS/ex12.js
@@ -14,4 +14,14 @@
 // alpha() 
 // console.log(z);   
 
- 
+ function sum(a,b){
+    
+    let total=0;
+    // for(i=0;i<arguments.length;i++)
+    //     {
+    //         total+=arguments[i];
+    //     }
+   arguments.length;
+   console.log(arguments)
+ }  
+console.log(sum());
