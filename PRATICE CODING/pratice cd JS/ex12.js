5f4cd09b6ce908095f060d111b18d26fa7c49351
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-06-28 19:57:17 +0530

    comment the all program

--- PRATICE CODING/pratice cd JS/ex12.js
+++ PRATICE CODING/pratice cd JS/ex12.js
@@ -1,135 +1,135 @@
-//                         // Arbitrary Function in JavaScript  
+// //                         // Arbitrary Function in JavaScript  
 
-// //  function sum(a,b){
+// // //  function sum(a,b){
     
-// //     let total=0;
-// //     for(i=0;i<arguments.length;i++)
-// //         {
-// //             total+=arguments[i];
-// //         }
-// // return total
-// //  }  
-// // console.log(sum(11,2,21,1,1 ));
+// // //     let total=0;
+// // //     for(i=0;i<arguments.length;i++)
+// // //         {
+// // //             total+=arguments[i];
+// // //         }
+// // // return total
+// // //  }  
+// // // console.log(sum(11,2,21,1,1 ));
 
 
-//                         // Function as Expression in JavaScript 
+// //                         // Function as Expression in JavaScript 
 
-// // const add=function(a,b){
-// //     console.log(a+b)
-// //     return a+b;
+// // // const add=function(a,b){
+// // //     console.log(a+b)
+// // //     return a+b;
+// // // }
+
+// // // add();
+// // // add(12,15);
+// // // console.log(add(12,15))
+
+
+// //                                     // Arrow function
+// // const add=()=>{
+
+// //     console.log("hello world")
 // // }
 
-// // add();
-// // add(12,15);
-// // console.log(add(12,15))
+// // add();    
 
 
-//                                     // Arrow function
-// const add=()=>{
+// //                                     // Default Parameter
 
-//     console.log("hello world")
+// // function  my2funciton(name="sabari"){
+// //     console.log("hi",name);
+// //   }
+// //   my2funciton("prasath");
+// //   my2funciton();
+
+//                                     // Recursion - function calling itself 
+// function factorial(n){
+     
+//   if(n==1)
+//   return n*factorial(n-1);
+// }
+// factorial();
+
+//             // --------------------------- Types of Functions in JavaScirpt
+
+
+// // Function Declaration
+// function normalFunction(){
+//     console.log("1. Function Declaration")
+// }
+// normalFunction();
+
+// // Function with return
+// function functionWithReturn(){
+
+//     return console.log("2. Function with return "); 
+// }
+// functionWithReturn();   
+
+// //Function as Expression (or) Variable Function
+// const functionExpression=function(){
+//     console.log("3. Function as Expression")
+// }
+// functionExpression();
+
+// // Arrow Function
+// const arrowFunction=()=>{
+//     console.log("4. Arrow Function")
 // }
 
-// add();    
+// arrowFunction();
 
 
-//                                     // Default Parameter
+//                                  //   Pratice functions
 
-// function  my2funciton(name="sabari"){
-//     console.log("hi",name);
+// function isPositive(num){
+//     if(num>0){
+//         console.log("isPositive");
+//     }
+//     else{
+//         console.log("isNegative");
+//     }
+// }
+// isPositive(1);
+// isPositive(-4);
+
+
+// // Question : get the values in array to sum the values using arrow function 
+// let arr=[1,2,3,4,5];
+// let sum=0;
+// const sumofarry=(arr)=>{
+//   for(let val of arr){
+//     console.log(val);
 //   }
-//   my2funciton("prasath");
-//   my2funciton();
 
-                                    // Recursion - function calling itself 
-function factorial(n){
-     
-  if(n==1)
-  return n*factorial(n-1);
-}
-factorial();
-
-            // --------------------------- Types of Functions in JavaScirpt
-
-
-// Function Declaration
-function normalFunction(){
-    console.log("1. Function Declaration")
-}
-normalFunction();
-
-// Function with return
-function functionWithReturn(){
-
-    return console.log("2. Function with return "); 
-}
-functionWithReturn();   
-
-//Function as Expression (or) Variable Function
-const functionExpression=function(){
-    console.log("3. Function as Expression")
-}
-functionExpression();
-
-// Arrow Function
-const arrowFunction=()=>{
-    console.log("4. Arrow Function")
-}
-
-arrowFunction();
-
-
-                                 //   Pratice functions
-
-function isPositive(num){
-    if(num>0){
-        console.log("isPositive");
-    }
-    else{
-        console.log("isNegative");
-    }
-}
-isPositive(1);
-isPositive(-4);
-
-
-// Question : get the values in array to sum the values using arrow function 
-let arr=[1,2,3,4,5];
-let sum=0;
-const sumofarry=(arr)=>{
-  for(let val of arr){
-    console.log(val);
-  }
-
-}
-sumofarry(arr);
+// }
+// sumofarry(arr);
 
 
 
-// Question 2 : find the area of the circle;
+// // Question 2 : find the area of the circle;
 
-const area=r=> Math.PI*r*r
-console.log(area(1)); 
+// const area=r=> Math.PI*r*r
+// console.log(area(1)); 
 
-// Question 3 : Adding the Multiple numbers using arrow function
+// // Question 3 : Adding the Multiple numbers using arrow function
 
-let multsum=function(){
-let total=0;
-    for(i=0;i<arguments.length;i++){
-        total+=arguments[i];
-    }
-    return total;
+// let multsum=function(){
+// let total=0;
+//     for(i=0;i<arguments.length;i++){
+//         total+=arguments[i];
+//     }
+//     return total;
 
-}
-console.log(multsum(12,12));
+// }
+// console.log(multsum(12,12));
 
 
-//callback function
-const part1=(val)=>{
-    console.log(val);
-}
-const arr2=['prasath','sabari','priya'];
-  result=arr2.forEach(part1);
+// //callback function
+// const part1=(val)=>{
+//     console.log(val);
+// }
+// const arr2=['prasath','sabari','Abi'];
+//   result=arr2.forEach(part1);
 
 
 
