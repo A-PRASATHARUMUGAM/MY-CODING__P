529080f5d51813bee8c7264377607b9c1c09aa48
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-06-28 19:45:56 +0530

    finished all function in js

--- PRATICE CODING/pratice cd JS/ex12.js
+++ PRATICE CODING/pratice cd JS/ex12.js
@@ -1,35 +1,101 @@
-                        // Arbitrary Function in JavaScript  
+//                         // Arbitrary Function in JavaScript  
 
-//  function sum(a,b){
+// //  function sum(a,b){
     
-//     let total=0;
-//     for(i=0;i<arguments.length;i++)
-//         {
-//             total+=arguments[i];
-//         }
-// return total
-//  }  
-// console.log(sum(11,2,21,1,1 ));
+// //     let total=0;
+// //     for(i=0;i<arguments.length;i++)
+// //         {
+// //             total+=arguments[i];
+// //         }
+// // return total
+// //  }  
+// // console.log(sum(11,2,21,1,1 ));
 
 
-                        // Function as Expression in JavaScript 
+//                         // Function as Expression in JavaScript 
 
-// const add=function(a,b){
-//     console.log(a+b)
-//     return a+b;
+// // const add=function(a,b){
+// //     console.log(a+b)
+// //     return a+b;
+// // }
+
+// // add();
+// // add(12,15);
+// // console.log(add(12,15))
+
+
+//                                     // Arrow function
+// const add=()=>{
+
+//     console.log("hello world")
 // }
 
-// add();
-// add(12,15);
-// console.log(add(12,15))
+// add();    
 
-// Arrow function
 
-const add=()=>{
+//                                     // Default Parameter
 
-    console.log("hello world")
+// function  my2funciton(name="sabari"){
+//     console.log("hi",name);
+//   }
+//   my2funciton("prasath");
+//   my2funciton();
+
+                                    // Recursion - function calling itself 
+function factorial(n){
+     
+  if(n==1)
+  return n*factorial(n-1);
+}
+factorial();
+
+            // --------------------------- Types of Functions in JavaScirpt
+
+
+// Function Declaration
+function normalFunction(){
+    console.log("1. Function Declaration")
+}
+normalFunction();
+
+// Function with return
+function functionWithReturn(){
+
+    return console.log("2. Function with return "); 
+}
+functionWithReturn();   
+
+//Function as Expression (or) Variable Function
+const functionExpression=function(){
+    console.log("3. Function as Expression")
+}
+functionExpression();
+
+// Arrow Function
+const arrowFunction=()=>{
+    console.log("4. Arrow Function")
 }
 
- 
- 
-add();    
+arrowFunction();
+
+
+                                 //   Pratice functions
+
+function isPositive(num){
+    if(num>0){
+        console.log("isPositive");
+    }
+    else{
+        console.log("isNegative");
+    }
+}
+isPositive(1);
+isPositive(-4);
+
+
+
+
+
+
+
+  
