6ead65054d4d0111242c30b18e0d7f73aed76ec4
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-06-30 16:45:14 +0530

    not finish the calculator because not enough

--- PRATICE CODING/pratice cd JS/ex16.js
+++ PRATICE CODING/pratice cd JS/ex16.js
@@ -5,19 +5,16 @@ calresult.id="calresult";
 let calbox=document.querySelector('.calbox').appendChild(calresult);
 
 calbutton.addEventListener('click',()=>{
-    let calinput=Number(document.querySelector(".calinput").value);
-    calresult.innerHTML="";
-    
-});
-
-let calculator="10+11";
-add=calculator.split('+');
-if(calculator.includes("+")){
-    for(val of add){
-        add1=Number(val[0]);
-         console.log(add1);
-    }
-}
+    let calinput=document.querySelector(".calinput").value;
+    calresult.innerHTML=calinput;
    
 
     
+
+    
+}); 
+
+
+
+ 
+     
