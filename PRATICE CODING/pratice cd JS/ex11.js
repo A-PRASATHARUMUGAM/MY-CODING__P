1b48d6fa1d586c319ccc18228bdd66c7c5db0505
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-07-04 21:52:06 +0530

    agian comment the template literals

--- PRATICE CODING/pratice cd JS/ex11.js
+++ PRATICE CODING/pratice cd JS/ex11.js
@@ -13,31 +13,30 @@
 // console.log("using backtick :"+full_name);  
  
 
-                // Long literal string 
+//                 // Long literal string 
 
-//Method -- 1   print the one line 
-let longString="variable is placeholder "
-+ "it is hold the value from variable name"
-+" agian using to assign the values to calculate then reassigining the values and that"
-+" is a main reason to using"
-+" the variable i  programming language";
-console.log(longString);
+// //Method -- 1   print the one line 
+// let longString="variable is placeholder "+"it is hold the value from variable name"
+// +" agian using to assign the values to calculate then reassigining the values and that"
+// +" is a main reason to using"
+// +" the variable i  programming language";
+// console.log(longString);
 
-//Method -- 2    print the one line 
-longString="variable is placeholder \
-it is hold the value from variable name \
-agian using to assign the values to calculate then reassigining the values and that \
-is a main reason to using \
-the variable i  programming language";
-console.log(longString);
+// //Method -- 2    print the one line 
+// longString="variable is placeholder \ it is hold \
+// the value from variable name \
+// agian using to assign the values to calculate then reassigining the values and that \
+// is a main reason to using \
+// the variable i  programming language";
+// console.log(longString);
  
-//Method --3   print the below format
-longString=`variable is placeholder
-it is hold the value from variable name
-agian using to assign the values to calculate then reassigining the values and that
-is a main reason to using
-the variable i  programming language`;
-console.log(longString);
+// //Method --3   print the below format
+// longString=`variable is placeholder
+// it is hold the value from variable name
+// agian using to assign the values to calculate then reassigining the values and that
+// is a main reason to using
+// the variable i  programming language`;
+// console.log(longString);
 
 // // Print the ASCII value 
 // let ASCII;
