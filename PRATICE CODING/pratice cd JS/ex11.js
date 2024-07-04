63a4a21ba5ed4918e229e4ed5aa68df468c83672
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-07-04 21:18:58 +0530

    comment again the long literal string

--- PRATICE CODING/pratice cd JS/ex11.js
+++ PRATICE CODING/pratice cd JS/ex11.js
@@ -10,37 +10,38 @@
 
 // // using backtick
 // full_name=`${first_name} ${last_name}`
-// console.log("using backtick :"+full_name);
+// console.log("using backtick :"+full_name);  
  
 
-//                 // Long literal string 
+                // Long literal string 
 
-// //Method -- 1 
-// let longString="variable is placeholder"
-// + "it is hold the value from variable name"
-// +"agian using to assign the values to calculate then reassigining the values and that"
-// +"is a main reason to using"
-// +"the variable i  programming language";
-// console.log(longString);
+//Method -- 1   print the one line 
+let longString="variable is placeholder "
++ "it is hold the value from variable name"
++" agian using to assign the values to calculate then reassigining the values and that"
++" is a main reason to using"
++" the variable i  programming language";
+console.log(longString);
 
-// //Method -- 2
-// longString="variable is placeholder\
-// it is hold the value from variable name\
-// agian using to assign the values to calculate then reassigining the values and that\
-// is a main reason to using\
-// the variable i  programming language";
-// console.log(longString);
-
-// //Method --3 
-// longString=`variable is placeholder
-// it is hold the value from variable name
-// agian using to assign the values to calculate then reassigining the values and that
-// is a main reason to using
-// the variable i  programming language`;
-// console.log(longString);
+//Method -- 2    print the one line 
+longString="variable is placeholder \
+it is hold the value from variable name \
+agian using to assign the values to calculate then reassigining the values and that \
+is a main reason to using \
+the variable i  programming language";
+console.log(longString);
+ 
+//Method --3   print the below format
+longString=`variable is placeholder
+it is hold the value from variable name
+agian using to assign the values to calculate then reassigining the values and that
+is a main reason to using
+the variable i  programming language`;
+console.log(longString);
 
 // // Print the ASCII value 
 // let ASCII;
-// ASCII=(String.fromCharCode(65,66,67)); 
-// console.log(ASCII);
+// ASCII=String.fromCharCode(65,66,67); 
+// console.log("It is return ascii value ",ASCII);
 
+ 
