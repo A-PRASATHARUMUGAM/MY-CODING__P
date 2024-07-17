b8eeec19ea0e9129c891a5a816b2324a3a086e54
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-07-17 23:43:22 +0530

    practice set and map object in js

---
+++ PRATICE CODING/pratice cd JS/ex19.js
@@ -0,0 +1,112 @@
+
+// 1. Set object : -it collection of the value 
+//              -Values are unique
+
+// 1. Map object : -it collection of the key-value pairs
+//              -Values are unique
+
+
+
+array=[1,2,3,4,4,5,5];
+
+// Normal object 
+let obj=new Object(array)
+
+console.log("Object :",obj);
+console.table(obj);
+
+
+// 1.Set object   --> Avoid the repeating value
+myset=new Set(array)
+
+console.log("Set Object:",myset);
+console.table(myset);
+
+console.log("               Example for Set")
+
+// Example 
+myset2=new Set();
+myset2.add(2)
+myset2.add(2)
+myset2.add(2)
+myset2.add("prasath")
+myset2.add(4)
+console.log(myset2);
+console.log(myset2.delete(4));  //--> delete 4
+
+myset2.add({a:1,b:2}); //--> object repeating because
+myset2.add({a:1,b:3}); //   -it is " Reference Type "
+console.log(myset2);
+
+console.log(myset2.size);
+console.log(myset2.has(2));  
+
+arr2=Array.from(myset2)  //-->it convert to array
+console.log("changed array",arr2);
+
+
+
+
+//2. Map Object --> collection of key-value pairs
+console.log("                   Map"); 
+
+
+let map1=new Map();
+map1.set('a',1);  //--> syntax: ('key any', value any)
+map1.set('b',2);  
+map1.set('c',3);  
+map1.set('d',4);  
+
+console.table(map1); 
+console.log(map1); 
+
+console.log(map1.size);
+console.log(map1.has('a',1));   
+
+map1.delete('d')
+console.log("delete d ",map1) 
+ 
+
+// Example
+console.log("       for");
+
+//for
+for(let i of map1){
+    console.log(i)
+}
+
+
+// way 2
+for(let [k,v] of map1){
+    console.log("key",k,"value",v);
+}
+
+// way 3
+for(let k of map1.keys()){
+    console.log("key",k);
+}
+
+for(let k of map1.values()){
+    console.log("value",k); 
+}
+
+
+
+//forEach
+console.log("       forEach");
+
+map1.forEach((v,k)=>{
+    console.log("key",k,"value",v);
+})
+
+
+
+//2d array to map
+console.log("       2d Array");
+
+arr2d=[['a',1],['b','2']];
+
+map2=new Map(arr2d);
+console.log(map2);
+console.log(...map2); //--> spread operation to convert Array
+
