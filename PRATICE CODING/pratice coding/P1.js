75464621da9b19d5095bee368e4fed331f947e68
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-07-18 00:39:23 +0530

    practice the leetcode questions

--- PRATICE CODING/pratice coding/P1.js
+++ PRATICE CODING/pratice coding/P1.js
@@ -5,3 +5,24 @@
 // menu2.addEventListener('click',()=>{
 //    menuList.classList.toggle("showmenu");
 // });
+
+
+// let variable='abac'
+
+//  let map1=new Map();
+// map1.set(variable)
+//  for(val of map1){
+//     if(val.has(variable)){
+//             val =0;
+//     }else{
+//         count++ 
+//     }
+
+// }
+//   console.log(map1) 
+
+let variable="1111"
+ myset=new Set()
+let newvariable=Array.from(variable)
+myset.add(newvariable);
+console.log(myset) 
