c58187298ff957046f9a160b0c5d18abe5ccdbae
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-08-10 19:56:11 +0530

    remove landpage responsive and portfolio remove

--- Portfolio responsive/assets/js/script.js
+++
@@ -1,6 +0,0 @@
-function myfunction(){
-    const menu=document.querySelector(".hmenu");
-    const menuList=document.querySelector(".navbar ul");
-       menuList.classList.toggle("showmenu")
-    
-   }   
