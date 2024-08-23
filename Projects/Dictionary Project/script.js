1b29fd16c31d107d8f0a2040691c3699b27f9cc4
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-08-23 15:20:49 +0530

    bending dictionary project

--- Projects/Dictionary Project/script.js
+++ Projects/Dictionary Project/script.js
@@ -1,6 +1,6 @@
+// search words inputs
 const searchInput=document.querySelector(".searchInput");
 const searchButton=document.querySelector(".searchButton");
-
 
 searchInput.addEventListener("keypress",(event)=>{
     if (event.key === "Enter") {
@@ -18,5 +18,16 @@ searchButton.addEventListener("click",()=>{
     
  });
 
+// add words inputs
+const englishWords=document.querySelector(".english-input");
+const tamilWords=document.querySelector(".tamil-input");
+const addButton=document.querySelector(".add-button");
+const wordsContainer=document.querySelector(".words-container");
 
+addButton.addEventListener("click",()=>{
+    Ewords=englishWords.value;
+    Twords=tamilWords.value;
+    wordsContainer.appendChild `<p class="words-english">${Ewords}: <span class="words-tamil">${Twords}</span></p>`;
+    
+})
 
