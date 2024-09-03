1e9b9d7a49f2abcc669a79e2440aaf5c2b1996fd
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-09-03 15:41:56 +0530

    practice class, inheritance, overriding, methodchining

--- PRATICE CODING/Practice Coding js/ex21.js
+++ PRATICE CODING/Practice Coding js/ex21.js
@@ -1,26 +1,13 @@
 
-// export default class Book{
-//     constructor(data){
-//         this.name=data;
-//         this.author=data;
-//         this.age=data;
+                                    //1. set and get in class 
+
+// class Temperature{
+//     constructor(tempdata){
+//         this._temp=tempdata
 //     }
-//      book1(){
-//         console.log("book1",this.age)
-//      }
+
 // }
 
-// let reader1=new Book()
-// reader1.name="sabari"
-// reader1.author="prasath"
-// reader1.age=24;
-
-
-// export function data(){
-//     console.log("hello data function ")
-// }
-
-
-
-// import books from './ex20.js';
-// import {data } from './ex20.js';
+// let temp=new Temperature()
+// temp.tempdata=25;
+// console.log(temp)
---
+++ PRATICE CODING/Practice Coding js/ex22.js
@@ -0,0 +1,26 @@
+
+// export default class Book{
+//     constructor(data){
+//         this.name=data;
+//         this.author=data;
+//         this.age=data;
+//     }
+//      book1(){
+//         console.log("book1",this.age)
+//      }
+// }
+
+// let reader1=new Book()
+// reader1.name="sabari"
+// reader1.author="prasath"
+// reader1.age=24;
+
+
+// export function data(){
+//     console.log("hello data function ")
+// }
+
+
+
+// import books from './ex20.js';
+// import {data } from './ex20.js';
