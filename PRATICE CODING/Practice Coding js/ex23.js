66cb3063fe21114273835f9f2c72d39e3d255ae2
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-09-03 16:35:10 +0530

    practice Asynchronous and synchronous

--- PRATICE CODING/Practice Coding js/ex23.js
+++ PRATICE CODING/Practice Coding js/ex23.js
@@ -1,13 +1,23 @@
+                                // Synchronous and Asynchronous
+                                
+
+// console.log("Hello..."); // f1
+
+// function sync(){  
+
+//     console.log("step 1") // f3 
+//     console.log("step 2") // f4 
+//     console.log("step 3") // f5 
+// }
+
+// sync() // f2
+
+// console.log(" -- using setTimeout --") // f6
+
+// setTimeout(()=>console.log("step 1"),2000) // f7        //--> minimum time 
+// setTimeout(()=>console.log("step 2"),1000) // f8
+// setTimeout(()=>console.log("step 3"),3000) // f9
 
 
 
-
-
-
-
-
-
-
-
-
-                        //  Asynchronous in JavaScirpt
+// setInterval(()=>console.log('hello kandhan '),5000)// again and again print 
