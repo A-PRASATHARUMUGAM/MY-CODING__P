a25687415baaa151e1064022a1d56a51c30991c7
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-09-04 20:19:51 +0530

    practice async and await

---
+++ PRATICE CODING/Practice Coding js/ex27.js
@@ -0,0 +1,28 @@
+                            // Async and Await
+
+// //async  --> it always return Promise
+
+
+// const check=new Promise((resolve,reject)=>{
+//     if(true){
+//         setTimeout(resolve, 1000,"success");
+//        }else{
+//           reject("fail")
+//        }
+// });
+
+// async function asyncfun(){
+//     try{
+//         console.log("Hello")
+//         let res= await check;
+//         console.log(res);
+//         console.log("bye")
+//     }
+// catch(erro){
+//     console.log(erro)
+// }
+// }
+// asyncfun()
+
+// asyncfun().then(()=>console.log("success"))
+
