a2f894c039f8d3e764eb76232a68e5f9f3159aae
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-09-04 20:18:22 +0530

    practice Exception Handling

---
+++ PRATICE CODING/Practice Coding js/ex26.js
@@ -0,0 +1,24 @@
+
+                                    // Exception Handling
+
+// try{
+//     const num=prompt("Enter a number");
+//     if(num==''){
+     
+//         throw("cannot be empty");
+
+//     }else if(isNaN(num)){
+
+//         throw("Enter number only ");
+        
+//     }else{
+
+//         console.log(Number(num)**2);
+//     }
+// }
+// catch(error){   // -->receving throw 
+//    console.log(error)
+// }
+// finally{
+//     console.log('It is using to finally works')
+// }
