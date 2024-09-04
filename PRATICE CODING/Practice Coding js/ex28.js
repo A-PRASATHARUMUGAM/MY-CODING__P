36f9c92dbc2aefc6afa77e47ffbd80c9c90afa60
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-09-04 20:20:50 +0530

    practice JSON

---
+++ PRATICE CODING/Practice Coding js/ex28.js
@@ -0,0 +1,46 @@
+                        // JSON - JavaScript Object Notation 
+
+// // 1.create JSON 
+
+// // String
+// let json1="sabari";
+
+// // Number
+// let json2=32;
+
+// // Boolean
+// let json3=true;
+
+// // Array
+// let json4=[1,2,3,4,5];
+
+// // Object         --> include (" ") the key 
+// let json5={
+//     "name":"prasath",
+//     "age":21
+// }
+
+// // 2. spreate string to normal data
+// let json6= ` [
+//     {
+//     "name":"prasath",
+//     "age":35
+//     },
+//     {
+//     "name":"sabari",
+//     "age":21
+//     },
+//     {
+//     "name":"kandhan",
+//     "age":22
+//     } 
+// ]`;
+// console.log(json6);
+
+// //convert string data to normal data 
+// let normalData=JSON.parse(json6);
+// console.log(normalData);
+
+// //convert normal data to string data
+// let stringData=JSON.stringify(normalData);
+// console.log(stringData);
