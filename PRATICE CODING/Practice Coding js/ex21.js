ffdc2578668b86940b624450e79a14f8f34113fd
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-09-05 13:02:52 +0530

    perfect understanding to get and set method

--- PRATICE CODING/Practice Coding js/ex21.js
+++ PRATICE CODING/Practice Coding js/ex21.js
@@ -1,13 +1,43 @@
+                                    //1. get and set Method 
+ 
+/*
+1.get --> it is use to get the private varibale to access other name  
+2.set --> it is use to set the value to private varibale  
+*/
 
-                                    //1. set and get in class 
 
-// class Temperature{
-//     constructor(tempdata){
-//         this._temp=tempdata
-//     }
+class Temperature{
+        temp1=24
+        constructor(temp2){
+          this._temp=temp2; // private variable declare to (  _variableName )
+        }
+        // get and set as same name beacuse get value and set value 
+        get temp3(){  
+            return this._temp;
+        } 
+        set temp3(temp){
+          this._temp =temp;
+        }
+        
+}
 
-// }
+let tempObject=new Temperature(100)
 
-// let temp=new Temperature()
-// temp.tempdata=25;
-// console.log(temp)
+                                console.log("       -- Before call object  -- ")
+console.log(tempObject);
+                                console.log("")
+
+
+tempObject._temp=200; // you not assign private variabel --> using get method
+console.log("Directly change private variable :",tempObject._temp); // you not access private variabel directly 
+console.log(tempObject);
+                                console.log("")
+
+
+tempObject.temp3=300; // set value 
+console.log("set method to change private variable :",tempObject.temp3);//get value 
+console.log(tempObject);
+
+
+
+
