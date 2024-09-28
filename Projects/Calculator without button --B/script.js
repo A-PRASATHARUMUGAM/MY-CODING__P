63df1e71cc20795b7cdb7d8d073502b502f00698
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-09-28 10:03:19 +0530

    change folder and delete some files

---
+++ Projects/Calculator without button --B/script.js
@@ -0,0 +1,14 @@
+
+let calbutton=document.querySelector(".calbutton");
+let calresult=document.createElement('div');
+calresult.id="calresult";
+let calbox=document.querySelector('.calbox').appendChild(calresult);
+
+calbutton.addEventListener('click',()=>{
+    let calinput=document.querySelector(".calinput").value;
+    calresult.innerHTML=calinput;
+   
+
+    
+}); 
+    
