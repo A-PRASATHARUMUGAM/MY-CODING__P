f38744a0481ecac2add4ae1ec4322072110da9c3
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-09-28 11:13:08 +0530

    practice sync and async

--- PRATICE CODING/Practice Coding js/ex24.js
+++ PRATICE CODING/Practice Coding js/ex24.js
@@ -1,5 +1,7 @@
                                 // Synchronous and Asynchronous
                                 
+// 1. synchronous -- it is blocking - because 1 execution is finish and move next execution 
+// 2. Asynchronous -- it is non-blocking - because not waiting for execution setTimeout to continously executing 
 
 // console.log("Hello..."); // f1
 
@@ -12,12 +14,18 @@
 
 // sync() // f2
 
-// console.log(" -- using setTimeout --") // f6
+// console.log(" using setTimeout ") // f6
 
 // setTimeout(()=>console.log("step 1"),2000) // f7        //--> minimum time 
 // setTimeout(()=>console.log("step 2"),1000) // f8
-// setTimeout(()=>console.log("step 3"),3000) // f9
+// setTimeout(()=>console.log("step 3"),1000) // f9 
 
 
 
-// setInterval(()=>console.log('hello kandhan '),5000)// again and again print 
+// setInterval(()=>console.log('hello sabari '),5000)// again and again print 
+
+
+
+
+
+
