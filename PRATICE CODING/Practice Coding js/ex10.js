f8b0d06a01ca76e3fc4c96854ac70d9004c37ec6
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-10-02 12:54:54 +0530

    change the project folder

--- PRATICE CODING/Practice Coding js/ex10.js
+++
@@ -1,4 +0,0 @@
-// let first_name="prasath"
-// c=first_name[Math.floor(Math.random()*7+0)];
-// console.log(c);
- 
---
+++ Projects/Random words in your name/script.js
@@ -0,0 +1,4 @@
+let first_name="prasath"
+c=first_name[Math.floor(Math.random()*7+0)];
+console.log(c);
+ 
