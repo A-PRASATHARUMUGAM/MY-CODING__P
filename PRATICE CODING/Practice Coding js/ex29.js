36d499daa932baae2a436dce3626c6a421c9c6fa
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-10-02 21:23:39 +0530

    practice json

--- PRATICE CODING/Practice Coding js/ex29.js
+++ PRATICE CODING/Practice Coding js/ex29.js
@@ -1,8 +1,23 @@
                         // JSON - JavaScript Object Notation 
 
+/*  1. JSON  -JavaScript Object Notation 
+    2. You can create object in Json include key to string format
+    3. Default Json format is array of object
+        {"employee":[
+            {"firstname":"prasath" ,"lastname":"Arumugam"}
+            {"firstname":"prasath" ,"lastname":"Arumugam"}
+            {"firstname":"prasath" ,"lastname":"Arumugam"}
+        ]}
+    4. JSON.parse(object)  -- it is string to array of object convert
+       JSON.strigify(object)  -- it is array of object to string convert
+
+  */ 
+                        
+
+
 // // 1.create JSON 
 
-// // String
+// // String 
 // let json1="sabari";
 
 // // Number
@@ -14,14 +29,13 @@
 // // Array
 // let json4=[1,2,3,4,5];
 
-// // Object         --> include (" ") the key 
+// // Object         --> include (" ") in key and value
 // let json5={
 //     "name":"prasath",
 //     "age":21
 // }
-
-// // 2. spreate string to normal data
-// let json6= ` [
+// // 2.  JSON Data seperate array of object 
+// let json= ` [
 //     {
 //     "name":"prasath",
 //     "age":35
@@ -35,12 +49,12 @@
 //     "age":22
 //     } 
 // ]`;
-// console.log(json6);
+// console.log(json);
 
-// //convert string data to normal data 
-// let normalData=JSON.parse(json6);
-// console.log(normalData);
+// //JSON.parse() string  to array of object   
+// let jsonparse=JSON.parse(json);
+// console.log(jsonparse);
 
-// //convert normal data to string data
-// let stringData=JSON.stringify(normalData);
-// console.log(stringData);
+// //JSON.stringify() array of object  to string    
+// let jsonsrignify=JSON.stringify(jsonparse);
+// console.log(jsonsrignify);
