1804337172aa80a72f97fb405fd2d2e289d9722f
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-10-03 19:56:40 +0530

    change project folder name

---
+++ Projects/fahrenheit to celsius converter/script.js
@@ -0,0 +1,10 @@
+
+function convert1(){
+
+    var celsius=document.querySelector("#celsius-input").value;
+     var fahrenheit=(celsius*9/5)+32;
+        fahresult=document.getElementById("fahresult");
+        fahresult.innerHTML = fahrenheit; 
+
+}   
+
