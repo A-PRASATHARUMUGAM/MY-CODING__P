b972ee9b8a405b533e138683637a04ae1fdddaa6
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-10-05 00:07:40 +0530

    added some comment to install React Extension

--- React.js/Setup React.js/script.jsx
+++ React.js/Setup React.js/script.jsx
@@ -1,10 +1,20 @@
 /*
-
 1. React    - it is javaScript Libaray to building userinterface
             - it is reduce the DOM code 
             - 3 important things 
                     -- component
                     -- prop
                     -- state
+
+*/ 
+
+/*                      --  Setup React.js --
+
+1. Install node.js - Recommended for most users LTS
+2. Install React Developer Tools Extension in Chrome Browser
+3. Install React Extension in Vscode  
+
+
+ 
 
 */ 
