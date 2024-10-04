e06926347a536b95faa225f1d2a7874bb49905f6
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-10-04 23:57:19 +0530

    rename the folder then add some comments

---
+++ React.js/Setup React.js/script.jsx
@@ -0,0 +1,10 @@
+/*
+
+1. React    - it is javaScript Libaray to building userinterface
+            - it is reduce the DOM code 
+            - 3 important things 
+                    -- component
+                    -- prop
+                    -- state
+
+*/ 
