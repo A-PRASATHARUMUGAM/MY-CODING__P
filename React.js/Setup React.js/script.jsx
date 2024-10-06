9cb567d0b4172a582c735bc3a8e181ba2fa84827
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-10-06 21:54:11 +0530

    finished react setup

--- React.js/Setup React.js/script.jsx
+++ React.js/Setup React.js/script.jsx
@@ -3,18 +3,13 @@
             - it is reduce the DOM code 
             - 3 important things 
                     -- component
-                    -- prop
-                    -- state
-
-*/ 
-
-/*                      --  Setup React.js --
+                    -- props s --
 
 1. Install node.js - Recommended for most users LTS
 2. Install React Developer Tools Extension in Chrome Browser
 3. Install React Extension in Vscode  
-
-
- 
-
+4. Install Node.js 
+5. go to terminal check the node -v 
+6. type this command --> npx create-react-app  yourfilename 
+7. you npm is notupdate you can update --> npm install -g npm@latest
 */ 
