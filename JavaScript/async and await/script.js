b2ea175fd9b5f3f5326e48c3c95be95dcdb3b1de
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-10-08 22:39:03 +0530

    delete current file and changed location in async and await

---
+++ JavaScript/async and await/script.js
@@ -0,0 +1,29 @@
+                            // Async and Await
+
+// async  --> it always return Promise
+
+
+function API(){
+return newpromise=new Promise((resolve,reject)=>{
+    if(true){
+        setTimeout(resolve, 3000,"success");
+       }else{
+          reject("fail")
+       }
+}); 
+}
+async function asyncfun(){
+    try{
+        console.log("Hello")
+        let result= await API();
+        console.log(result);
+
+        console.log("bye")
+    }
+catch(erro){
+    console.log(erro)
+}
+}
+asyncfun()
+
+
