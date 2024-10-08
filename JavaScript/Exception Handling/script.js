deb00420dcda50795eab2cbf178ac44bbbff5a16
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-10-08 22:31:00 +0530

    change file loaction and delete it

--- JavaScript/Exception Handling/script.js
+++ JavaScript/Exception Handling/script.js
@@ -1,27 +1,26 @@
 
                                     // Exception Handling
 
-                                    try{
-                                        const num=prompt("Enter a number");
-                                        if(num==''){
-                                         
-                                            throw("cannot be empty");
-                                    
-                                        }else if(isNaN(num)){
-                                    
-                                            throw("Enter number only ");
-                                            
-                                        }else{
-                                    
-                                            console.log(Number(num)**2);
-                                        }
-                                    }
-                                    catch(error){   // -->receving throw 
-                                       console.log(error)
-                                    }
-                                    finally{
-                                        console.log("It is using file closing work finally")
-                                    }
-                                    
-                                    
-                                    
+try{
+    const num=prompt("Enter a number");
+    if(num==''){
+     
+        throw("cannot be empty");
+
+    }else if(isNaN(num)){
+
+        throw("Enter number only ");
+        
+    }else{
+
+        console.log(Number(num)**2);
+    }
+}
+catch(error){   // -->receving throw 
+   console.log(error)
+}
+finally{
+    console.log("It is using file closing work finally")
+}
+
+
