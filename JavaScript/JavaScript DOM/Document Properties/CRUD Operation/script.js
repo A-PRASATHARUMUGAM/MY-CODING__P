e139c978191c8e189dffba6253eb749878916f04
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-10-09 08:48:59 +0530

    practice createElement in DOM

--- JavaScript/JavaScript DOM/Document Properties/CRUD Operation/script.js
+++ JavaScript/JavaScript DOM/Document Properties/CRUD Operation/script.js
@@ -1,1 +1,31 @@
                             // CRUD Operations
+
+
+/*
+    1. createAttribute ()       - use
+    2. createAttributeNS()
+    3. createCDATASection()
+    4. createComment()          - use
+    5. createDocumentFragment()
+    6. createElement()          - use
+    7. createElementNS()
+    8. createEvent()
+    9. createExpression()
+    10. createNSResolver()
+    11. createNodeIterator()
+    21. createProcessingInstruction()
+    13. createNodeIterator()
+    14. createRange()
+    15. createTextNode()          - use
+    16. createTreeWalker()
+
+*/
+
+
+
+// Create Element
+let h2=document.createElement('h2');
+h2Text=document.createTextNode("hello world ");
+h2.append(h2Text);
+let main=document.querySelector(".main-container");
+main.append(h2)
