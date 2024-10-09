bb6303f31a6d57c42dde4f6573553de5be1cade9
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-10-09 06:07:57 +0530

    practice getElementsByClassName

--- JavaScript/JavaScript DOM/DOM - Properties/getElementByClassName/script.js
+++ JavaScript/JavaScript DOM/DOM - Properties/getElementByClassName/script.js
@@ -1,1 +1,18 @@
                         // getElementByClassName
+
+
+
+
+let softwareRequirement=document.getElementsByClassName("req");
+
+console.log(softwareRequirement[0].innerText = "CPU");
+
+
+
+
+// other way to access it means --> softwareContainer > child access 
+// 1. the container to put ID to access 
+let softwareContainer=document.getElementById("software-container");
+
+let softwareItems =softwareContainer.getElementsByClassName("req");
+console.log(softwareItems[2].innerText ="Processor")
