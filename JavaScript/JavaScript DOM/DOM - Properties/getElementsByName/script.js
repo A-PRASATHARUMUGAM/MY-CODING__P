79626ffcdb073ad72cdf69172ec9e9ee2a5b2338
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-10-09 06:35:29 +0530

    practice getElementsByName

---
+++ JavaScript/JavaScript DOM/DOM - Properties/getElementsByName/script.js
@@ -0,0 +1,9 @@
+                                        // getElementsByName
+
+// ! important can't get  "username" only  
+let formData1=document.getElementsByName("username1");
+console.log(formData1)
+
+
+let formData2=document.getElementsByName("password");
+console.log(formData2[0])
