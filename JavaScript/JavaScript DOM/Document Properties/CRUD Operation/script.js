b7b9597b6deb5b058baae95c8a4461b6cba21bd3
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-10-10 05:46:03 +0530

    add some DOM methods and practice createtextnode

--- JavaScript/JavaScript DOM/Document Properties/CRUD Operation/script.js
+++ JavaScript/JavaScript DOM/Document Properties/CRUD Operation/script.js
@@ -19,13 +19,68 @@
     15. createTextNode()          - use
     16. createTreeWalker()
 
+
+
+    
+1. setAttribute()
+2. setAttributeNS()
+3. setAttributeNode()
+4. setAttributeNodeNS()
+5. setHTMLUnsafe()
+6. setPointerCapture()
+
+7. getAnimations()
+8. getAttribute()
+9. getAttributeNS()
+10. getAttributeNames()
+11. getAttributeNode()
+12. getAttributeNodeNS()
+13. getBoundingClientRect()
+14. getClientRects()
+
+15. releasePointerCapture()
+16. remove()
+17. removeAttribute()
+18. removeAttributeNS()
+19. removeAttributeNode()
+21. removeChild()
+22. removeEventListener()
+23. replaceChild()
+24. replaceChildren()
+25. replaceWith()
+26. requestFullscreen()
+27. requestPointerLock()
+
+
+
 */
 
 
 
-// Create Element
+// createElement
+let textNode1=document.createTextNode("hello world ");
 let h2=document.createElement('h2');
-h2Text=document.createTextNode("hello world ");
-h2.append(h2Text);
+h2.append(textNode1);  // --> only one time store the textNode
 let main=document.querySelector(".main-container");
 main.append(h2)
+main.prepend(h2)
+ 
+// createComment
+let comment=document.createComment("DOM via comment");
+main.append(comment);
+
+// createTextNode
+let textNode2=document.createTextNode("Every day is a chance to be the best");
+let para1=document.createElement('p');
+para1.append(textNode2);    // --> only one time store in the textNode
+main.prepend(para1);
+
+                        // !important
+// text=document.textContent="hello"; --> you can store multiple time in the text
+
+
+// createAttribute
+let attribut1=document.createAttribute("width","20px")
+console.log(attribut1)
+
+
