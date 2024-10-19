30312c9e40f7c51a5597c8fa36db378116e5d982
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-10-19 15:04:57 +0530

    not proper finished

--- Projects/Add cort/script.js
+++ Projects/Add cort/script.js
@@ -1,8 +1,38 @@
-const headerLinks=document.querySelector(".header-links");
+const headerLinks=document.querySelector(".header-links"); 
 const hamurgerMenu=document.querySelector(".hamburger-menu");
-hamurgerMenu.addEventListener("click",()=>{
+const cartAdd=document.querySelector(".cart-box");
+const cartRemoveEl=document.querySelector(".remove-icon");
+const quantEl=document.querySelectorAll("input");
+quantEl.forEach((input)=>{
 
-headerLinks.classList.toggle("header-links-active")
-hamurgerMenu.classList.toggle("Ahmenu")
+   input.addEventListener("change",quantfunction)
 
 })
+function quantfunction(){
+
+        if(isNaN(this.value) || this.value< 1 ){
+            this.value=1
+        }
+        
+         console.log(this.parentElement);
+    
+}
+
+
+
+// hamurgerMenu.addEventListener("click",()=>{
+//     headerLinks.classList.toggle("header-links-active")
+//     hamurgerMenu.classList.toggle("Ahmenu")
+//     cartAdd.classList.add("cart-box-active");
+
+
+// })
+// cartRemoveEl.addEventListener("click",()=>{
+//     cartAdd.classList.remove("cart-box-active");
+
+    
+// })
+
+//           <input type="number" value="1" min="1">
+
+
