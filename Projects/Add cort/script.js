0e751eb51d7af3d01d4d67ba044868015e870fcd
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-10-19 14:07:24 +0530

    add some image  and navstructure

---
+++ Projects/Add cort/script.js
@@ -0,0 +1,8 @@
+const headerLinks=document.querySelector(".header-links");
+const hamurgerMenu=document.querySelector(".hamburger-menu");
+hamurgerMenu.addEventListener("click",()=>{
+
+headerLinks.classList.toggle("header-links-active")
+hamurgerMenu.classList.toggle("Ahmenu")
+
+})
