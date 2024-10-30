5fcb588a30edea3d17f246f6b1fe07a67a0a02aa
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-10-30 16:18:32 +0530

    delete folder and change the folder location

--- Portfolio_Prasath_Responsive/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-const headerLinks=document.querySelector(".header-links");
-const hamurgerMenu=document.querySelector(".hamburger-menu");
-hamurgerMenu.addEventListener("click",()=>{
-
-headerLinks.classList.toggle("showmenu")
-hamurgerMenu.classList.toggle("Ahmenu")
-
-})
