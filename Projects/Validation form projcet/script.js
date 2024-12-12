685ea54f4e4cc89e93a0626c4e841859bb1cfc3e
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-12-12 11:17:02 +0530

    fully finished structrue

---
+++ Projects/Validation form projcet/script.js
@@ -0,0 +1,13 @@
+const ErrorMessage=document.querySelector(".error");
+const InputTag=document.querySelectorAll("input");
+const FormButton=document.querySelector(".form-btn-container");
+
+
+for( val of InputTag ){
+    val.addEventListener("click",()=>{ 
+
+        ErrorMessage.innerHTML="Error" 
+        
+})  
+}
+
