ee31771d5d6a4c9efbbb88cc799c8f6083c0a306
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-12-21 09:23:42 +0530

    practice inline and external style sheet in React

--- React/react-main-file/src/Footer.jsx
+++ React/react-main-file/src/Footer.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 
 const Footer = () => {
-    const name= "prasath"
+ const name="prasath";
+
+ function myfunction(){
+
+    const name= ["prasath", "sabari", "kandhan"]; 
+         let rand=Math.floor(Math.random()*3);    
+         return name[rand]       
+ 
+          
+     
+ } 
 
   return (
-    <div>Footer</div>
-     <p>hello{name}</p>
+    
+    <div className="Footer">
+             
+    <p>{name}</p> 
+    <p>hello {myfunction()}</p> 
+
+    </div>
+  
 
   )
 }
