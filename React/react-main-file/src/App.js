7d9d42a9da54e7e2e342f7f5ea7dd6694e0331d8
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2024-12-24 23:23:56 +0530

    add React_Hook component and corresponding CSS file

--- React/react-main-file/src/App.js
+++ React/react-main-file/src/App.js
@@ -3,6 +3,8 @@ import Header from './Header';
 import Footer from './Footer'; 
 import React_CSS  from './React Appling Style Sheet/React_CSS';
 import Click from './React Click Event/ClickEvent';  
+import React_Hook from './React Hook State/React_Hook';
+
 function App() {
   return (  
     <div className="App"> 
@@ -10,6 +12,7 @@ function App() {
       <Footer/> 
       <React_CSS/> 
       <Click/>    
+      <React_Hook/>
     </div>
   ); 
 }  
---
+++ React/react-main-file/src/React Hook State/React_Hook.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './React_Hook'
+const React_Hook = () => {
+  return (
+    <div>React_Hook</div>
+  )
+}
+
+export default React_Hook
+
+
+
+
+
+
+
+
